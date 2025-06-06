d6bd534247dda7af9043f8da629f78c33051ca9b
joaquimg <joaquimdgarcia@gmail.com>
2025-06-06 00:47:55 -0300

    Base code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-# Copyright (c) 2025: Joaquim Garcia, Oscar Dowson and contributors
+# Copyright (c) 2025: Joaquim Dias Garcia, Oscar Dowson and contributors
 #
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
