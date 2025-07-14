f93318cf4411a26d92376f451a02947df99aba1e
Documenter.jl <documenter@juliadocs.github.io>
2025-07-14 03:24:49 +0000

    build based on c76894b

---
+++ previews/PR5/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MathOptIIS.jl","page":"MathOptIIS.jl","title":"MathOptIIS.jl","text":"","category":"section"},{"location":"","page":"MathOptIIS.jl","title":"MathOptIIS.jl","text":"This package is a basic conflict solver for MathOptInterface.jl.","category":"page"}]
+}
