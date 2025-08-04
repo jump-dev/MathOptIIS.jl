5a85c7b9e14357f8a445daa93b02829ce5a93696
odow <o.dowson@gmail.com>
2025-08-05 10:54:14 +1200

    Remove mention of documentation

--- docs/make.jl
+++
@@ -1,8 +0,0 @@
-using Documenter, MathOptIIS, JuMP
-
-makedocs(; sitename = "MathOptIIS.jl documentation")
-
-deploydocs(;
-    repo = "github.com/jump-dev/MathOptIIS.jl.git",
-    push_preview = true,
-)
